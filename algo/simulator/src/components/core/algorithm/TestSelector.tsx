a4d5721551b3b94f54e82762b2fc7fe85d11d8f7
VietCT <transonviet2004@gmail.com>
2025-09-11 01:49:18 +0800

    Added manage obstacles

--- algo/simulator/src/components/core/algorithm/TestSelector.tsx
+++ algo/simulator/src/components/core/algorithm/TestSelector.tsx
@@ -93,6 +93,15 @@ export const TestSelector = (props: TestSelectorProps) => {
       />
 
       {/* Manage Custom Obstacles */}
+      {
+        //selectedTestEnum === AlgoTestEnum.Custom
+        true && (
+          <Button onClick={() => setIsManageObstaclesModalOpen(true)}>
+            <span>Manage Obstacles</span>
+            <FaBox />
+          </Button>
+        )
+      }
 
       {/* Reset grid button */}
       <Button
